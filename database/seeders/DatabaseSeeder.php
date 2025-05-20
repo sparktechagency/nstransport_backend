fa59f5eae8f5257d4a53890300481c8d0cf5e85d
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2025-05-20 16:06:01 +0600

    modificaiton complete

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CustomerSeeder::class,
+            // CustomerSeeder::class,
             CategorySeeder::class,
             VahicleSeeder::class,
-            BookingSeeder::class,
+            // BookingSeeder::class,
             UserSeeder::class,
         ]);
     }
